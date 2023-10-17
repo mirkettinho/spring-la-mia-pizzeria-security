01066a600f586a5f0cad483a3a2a2047f60b7706
mirkettinho <mirkodifranco30@gmail.com>
2023-10-17 11:28:31 +0200

    first commit

---
+++ src/main/java/org/java/app/pizzeria/repo/OfferteRepo.java
@@ -0,0 +1,8 @@
+package org.java.app.pizzeria.repo;
+
+import org.java.app.pizzeria.pojo.Offerte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfferteRepo extends JpaRepository<Offerte, Integer> {
+
+}
